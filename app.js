5335cc3522bfadbb8d233ee5285a96fb679662c5
Franko Stipanov <fstipanov@gmail.com>
2021-05-31 11:20:53 +0200

    add app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// console.log(document.querySelector('.message'));
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent = '🎉Correct Number!';
+// document.querySelector('.number').textContent = '10';
+// document.querySelector('.score').textContent = '12';
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No Number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉Correct Number!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = '📈 Too High!';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '📉 Too Low!';
+  }
+});
